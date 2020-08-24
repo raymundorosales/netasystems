ffdd8fa587f36218c1e307affbb4208a5907db48
raymundorosales <raymundorosaleshdz@gmail.com>
2020-08-24 17:02:13 -0500

    actualizacion de comentarios de clase UsuarioDao

--- src/main/java/com/service/UserDao.java
+++ src/main/java/com/service/UserDao.java
@@ -19,9 +19,8 @@ public class UserDao implements IUserDao {
 	
 
 	/**
-	 * Metodo para obtener TODOS los datos de un registro de la tabla PERSONAS
+	 * Metodo para obtener TODOS los datos de un registro de la tabla NS_USERS
 	 * @param login se usara login en caso de que se este buscando con el correo y el password del usuario
-	 * @param personaNo se usara este valor cuando se desee buscar por medio de la llave primaria USER_NO
 	 * @return login contendra los datos que se encuentren en el registro encontrado
 	 */
 	@Override
@@ -60,9 +59,9 @@ public class UserDao implements IUserDao {
 	}
 	
 	/**
-	 * Metodo para obtener la llave primaria USER_NO de la tabla PERSONAS buscando con el correo
+	 * Metodo para obtener la llave primaria USER_NO de la tabla NS_USERS buscando con el correo
 	 * @param username contendra el correo con el que queremos buscar el registro
-	 * @return personaNo devuelve la llave primaria obetenida, en caso de que no encuentre registro devolvera 0
+	 * @return userNo devuelve la llave primaria obetenida, en caso de que no encuentre registro devolvera 0
 	 */
 	@Override
 	public int searchIdByUsername(String username) {
@@ -73,14 +72,14 @@ public class UserDao implements IUserDao {
 			conn = dataSource.getConnection();
 			PreparedStatement ps = conn.prepareStatement(sql);
 			ps.setString(1, username);
-			int personaNo = 0;
+			int userNo = 0;
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
-				personaNo = rs.getInt("USER_NO");
+				userNo = rs.getInt("USER_NO");
 			}
 			rs.close();
 			ps.close();
-			return personaNo;
+			return userNo;
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		} finally {
@@ -95,7 +94,7 @@ public class UserDao implements IUserDao {
 	
 	/**
 	 * Metodo para buscar la ultima llave primaria USER_NO de la tabla personas e incrementarla en 1
-	 * @return personaNo es el valor de la siguiente llave primaria que se debe insertar en la tabla
+	 * @return userNo es el valor de la siguiente llave primaria que se debe insertar en la tabla
 	 */
 	@Override
 	public int searchLastId() {
@@ -105,17 +104,17 @@ public class UserDao implements IUserDao {
 		try {
 			conn = dataSource.getConnection();
 			PreparedStatement ps = conn.prepareStatement(sql);
-			int personaNo = 0, temp = 0;
+			int userNo = 0, temp = 0;
 			ResultSet rs = ps.executeQuery();
 			if (rs.next()) {
 				if (temp == 1)
 					rs.close();
-				personaNo = rs.getInt("USER_NO") + 1;
+				userNo = rs.getInt("USER_NO") + 1;
 				temp ++;
 			}
 			rs.close();
 			ps.close();
-			return personaNo;
+			return userNo;
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
 		} finally {
